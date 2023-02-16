6d21089fb4fdbd507e4fb91155a4e9eed30d21b0
Micael Cosmo <micael.trabalho@hotmail.com>
2023-02-16 11:44:09 -0300

    feat/refator: Colored Scatter

--- plt_show.py
+++ plt_show.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 
-x = [1, 2, 3, 4, 5]
+x = [1, 2, 4, 4, 5]
 y = [2, 3, 7, 1, 0]
+# x2 = [2, 4, 6, 8, 10]
+# y2 = [5, 1, 5, 7, 4]
 titulo = "Gráico de barras"
 eixo_x = "Eixo X"
 eixo_y = "Eixo Y"
@@ -9,7 +11,14 @@ eixo_y = "Eixo Y"
 plt.title(titulo)
 plt.xlabel(eixo_x)
 plt.ylabel(eixo_y)
-# plt.plot(x, y)
+
 # Gráfico de barras
-plt.bar(x, y)
+# plt.bar(x, y, label="Grupo 1")
+# plt.bar(x2, y2, label="Grupo 2")
+
+# Gráfico de scatter
+plt.scatter(x, y, label="Meus pontos", color="r")
+plt.plot(x, y, color="red")
+
+plt.legend()
 plt.show()
