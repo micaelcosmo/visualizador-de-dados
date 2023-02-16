b2f506ebd8acf0e346ce1857669675de2bc4d8b3
Micael Cosmo <micael.trabalho@hotmail.com>
2023-02-16 11:35:29 -0300

    feat/refactor: Bars

--- plt_show.py
+++ plt_show.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 
-x = [1, 2]
-y = [2, 3]
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 7, 1, 0]
+titulo = "Gráico de barras"
+eixo_x = "Eixo X"
+eixo_y = "Eixo Y"
 
-plt.title('Meu primeiro gráfico com matplotlib.')
-
-plt.xlabel("Eixo X")
-plt.ylabel("Eixo Y")
-
-plt.plot(x, y)
+plt.title(titulo)
+plt.xlabel(eixo_x)
+plt.ylabel(eixo_y)
+# plt.plot(x, y)
+# Gráfico de barras
+plt.bar(x, y)
 plt.show()
