e25d6dd1511915e08969902437ef39bf3de7d9c3
Micael Cosmo <micael.trabalho@hotmail.com>
2023-02-16 11:27:39 -0300

    feat: 'Hello World PLOT

---
+++ plt_show.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2]
+y = [2, 3]
+
+plt.title('Meu primeiro gráfico com matplotlib.')
+
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo Y")
+
+plt.plot(x, y)
+plt.show()
